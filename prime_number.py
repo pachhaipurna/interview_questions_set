46900dbe5953746c12d1f39e5189f636dae6ab5b
pachhaipurna <pachhaipurna290@gmail.com>
2021-09-04 11:19:23 +0900

    Prime no program added

---
+++ prime_number.py
@@ -0,0 +1,28 @@
+#Check no is prime or not 
+
+def check_prime_no(num):
+    flag =False
+
+    if num < 0:
+        print('PLease input positive no only')
+    
+    elif num ==1:
+        print('PLease add more than 1')
+    
+    else:    
+        if num >1:
+            for i in range(2, num):
+                if num % i ==0:
+                    flag =True
+                    break
+
+    if flag:
+        print('This is not prime no')
+    else:
+        print('This is prime no')
+
+input_val = (int(input('Enter any number')))
+
+
+check_prime_no(input_val)
+
